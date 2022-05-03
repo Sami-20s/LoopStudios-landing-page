14bffc0307d57b01a4e52fadf0fa679174e76141
Sami-20s <samikfouri22@gmail.com>
2022-05-03 16:44:21 +0300

    🧭🎗 Nav Style Added

---
+++ styles/NavStyle.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import { media } from "./screenSize";
+
+export const NavWrapper = styled.div`
+    padding: 40px 25px;
+    display: flex;
+    height: ${props => (props.isClicked ? "100vh" : "unset")};
+    background-color: ${props => (props.isClicked ? "black" : "red")};
+    justify-content: space-between;
+    align-items: flex-start;
+    overflow: none;
+
+    @media ${media.large} {
+        overflow: unset;
+        height: unset;
+        background-color: red;
+        padding: 60px 165px;
+    }
+`;
+
+export const NavLinksStyle = styled.div`
+    ul {
+        display: ${props => (props.isClicked ? "flex" : "none")};
+        flex-direction: column;
+        position: fixed;
+        top: 220px;
+        left: 25px;
+        @media ${media.large} {
+            left: 0;
+            top: 0;
+            display: flex;
+            flex-direction: row;
+            position: relative;
+        }
+        li {
+            font-family: var(--josefin);
+            font-weight: 300;
+            color: var(--white);
+            font-size: 1.5625rem;
+            @media ${media.large} {
+                position: relative;
+                font-family: var(--alata);
+                font-weight: 400;
+                font-size: 1.125rem;
+                cursor: pointer;
+                transition: all 0.3s ease;
+                &::after {
+                    display: block;
+                    position: absolute;
+                    content: "";
+                    left: 50%;
+                    transform: translateX(-50%);
+                    top: calc(100% + 10px);
+                    background-color: transparent;
+                    width: 24px;
+                    height: 2px;
+                    transition: all 0.3s ease;
+                }
+                &:hover {
+                    &::after {
+                        background-color: var(--white);
+                    }
+                }
+            }
+            &:not(:last-child) {
+                padding-bottom: 30px;
+                @media ${media.large} {
+                    padding-bottom: 0;
+                    margin-right: 32px;
+                }
+            }
+        }
+    }
+`;
+export const BurgerIconStyle = styled.div`
+    display: block;
+    @media ${media.large} {
+        display: none;
+    }
+    button {
+        background-color: transparent;
+        border: none;
+        outline: none;
+    }
+`;
